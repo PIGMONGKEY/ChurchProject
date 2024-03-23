5355d223e68e09b824394250f1364984d4238faa
LeeWonHee <pigmong02020202@gmail.com>
2024-03-23 13:56:33 +0900

    Initial Commit

---
+++ src/main/java/com/pyunggang/churchproject/ChurchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pyunggang.churchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchProjectApplication.class, args);
+    }
+
+}
