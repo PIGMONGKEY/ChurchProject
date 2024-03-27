5ee0ce34a0341af0beeb0d58ca24d335c03e0ed5
LeeWonHee <pigmong02020202@gmail.com>
2024-03-27 11:38:18 +0900

    RegisterController: TODO 추가

--- src/main/java/com/pyunggang/churchproject/controller/RegisterController.java
+++ src/main/java/com/pyunggang/churchproject/controller/RegisterController.java
@@ -8,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -31,6 +30,7 @@ public class RegisterController {
 
     @GetMapping("/list")
     public String registerHome(@RequestParam("church") String church, @RequestParam("event") String event, Model model) {
+//        TODO: 참가자 테이블 생성 후 불러와서 뿌려주기
         log.info(church + event);
         return "register/list";
     }
