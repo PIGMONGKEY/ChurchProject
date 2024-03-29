5080b5a835546f14102dc065c91d45cea606f11d
LeeWonHee <pigmong02020202@gmail.com>
2024-03-29 15:37:48 +0900

    ApplymentServiceImpl: 중복체크 추가 / Applyment 삽입 추가

--- src/main/java/com/pyunggang/churchproject/service/impl/ApplymentServiceImpl.java
+++ src/main/java/com/pyunggang/churchproject/service/impl/ApplymentServiceImpl.java
@@ -1,13 +1,12 @@
 package com.pyunggang.churchproject.service.impl;
 
 import com.pyunggang.churchproject.data.dto.ParticipantRegisterParam;
+import com.pyunggang.churchproject.data.entity.Applyment;
 import com.pyunggang.churchproject.data.entity.Participant;
-import com.pyunggang.churchproject.data.repository.ApplymentRepository;
-import com.pyunggang.churchproject.data.repository.ChurchRepository;
-import com.pyunggang.churchproject.data.repository.DepartmentRepository;
-import com.pyunggang.churchproject.data.repository.ParticipantRepository;
+import com.pyunggang.churchproject.data.repository.*;
 import com.pyunggang.churchproject.service.ApplymentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,11 +14,13 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ApplymentServiceImpl implements ApplymentService {
     final private ApplymentRepository applyRepo;
     final private ParticipantRepository partiRepo;
     final private ChurchRepository churchRepo;
     final private DepartmentRepository departRepo;
+    final private EventRepository eventRepo;
 
     /**
      * 참가자 정보 리스트로 받아서 insert
@@ -30,6 +31,7 @@ public class ApplymentServiceImpl implements ApplymentService {
     @Override
     public boolean saveApplyment(List<ParticipantRegisterParam> params) {
         Participant participant;
+        Applyment applyment;
 
         for (ParticipantRegisterParam param : params) {
             // 빈 데이터가 있는 참가자 정보 건너뛰기
@@ -53,6 +55,16 @@ public class ApplymentServiceImpl implements ApplymentService {
             } else {
                 // DB 삽입 실패하면 false 리턴
                 if (!partiRepo.save(participant).getName().equals(param.getName()))
+                    return false;
+
+                // 신청 생성
+                applyment = Applyment.builder()
+                        .event(eventRepo.findEventByNameIs(param.getEventName()))
+                        .participant(participant)
+                        .build();
+
+                // DB 삽입 실패하면 false 리턴
+                if (!applyRepo.save(applyment).getParticipant().getName().equals(param.getName()))
                     return false;
             }
         }
