474d54b7f9566322aae27c75a1e4a0a8e659ef75
LeeWonHee <pigmong02020202@gmail.com>
2024-04-02 14:21:09 +0900

    AdminController: Admin 페이지 진입 메서드 생성

--- src/main/java/com/pyunggang/churchproject/controller/AdminController.java
+++ src/main/java/com/pyunggang/churchproject/controller/AdminController.java
@@ -1,11 +1,9 @@
 package com.pyunggang.churchproject.controller;
 
-import ch.qos.logback.core.model.Model;
-import com.pyunggang.churchproject.data.repository.ChurchRepository;
-import com.pyunggang.churchproject.data.repository.DepartmentRepository;
-import com.pyunggang.churchproject.data.repository.EventRepository;
+import com.pyunggang.churchproject.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,12 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/admin/*")
 public class AdminController {
-    final EventRepository eventRepo;
-    final DepartmentRepository departRepo;
-    final ChurchRepository churchRepo;
+    final private AdminService adminService;
 
     @GetMapping("/")
     public String admin(Model model) {
+        model.addAttribute(adminService.getAdminPageInfo());
 
         return "/admin/admin";
     }
