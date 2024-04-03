68113fa38130e48114c730b1484627a95889607f
LeeWonHee <pigmong02020202@gmail.com>
2024-04-03 18:11:28 +0900

    SecurityConfig: Created

---
+++ src/main/java/com/pyunggang/churchproject/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.pyunggang.churchproject.config;
+
+import com.pyunggang.churchproject.jwt.JwtAuthenticationFilter;
+import com.pyunggang.churchproject.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    final private JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManager) -> sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorizeRequest) -> {
+                    authorizeRequest.requestMatchers("/register/start").permitAll();
+                    authorizeRequest.requestMatchers("/auth/login").permitAll();
+                    authorizeRequest.requestMatchers("/register/*").hasRole("USER");
+                    authorizeRequest.requestMatchers("/admin/*").hasRole("ADMIN");
+                })
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
