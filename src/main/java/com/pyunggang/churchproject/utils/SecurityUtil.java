5105ea6aaea0635a6634b311797244d257f28f40
LeeWonHee <pigmong02020202@gmail.com>
2024-04-04 17:31:33 +0900

    SecurityUtil: Created

---
+++ src/main/java/com/pyunggang/churchproject/utils/SecurityUtil.java
@@ -0,0 +1,16 @@
+package com.pyunggang.churchproject.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class SecurityUtil {
+    public static String getCurrentChurchName() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getName() == null) {
+            throw new RuntimeException("No authentication information");
+        }
+
+        return authentication.getName();
+    }
+}
