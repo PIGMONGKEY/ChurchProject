dfbe0af3efababb41b11b70f4ba4619bb29efad3
LeeWonHee <pigmong02020202@gmail.com>
2024-04-05 17:42:18 +0900

    SecurityConfig: 주석 추가

--- src/main/java/com/pyunggang/churchproject/config/SecurityConfig.java
+++ src/main/java/com/pyunggang/churchproject/config/SecurityConfig.java
@@ -27,14 +27,22 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement((sessionManager) -> sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorizeRequest) -> {
+                    // 교회 홈 접근
                     authorizeRequest.requestMatchers("/register/home").permitAll();
+                    // 교회 참가자 신청 접근
                     authorizeRequest.requestMatchers("/register/register").permitAll();
+                    // 로그인 페이지 접근
                     authorizeRequest.requestMatchers("/login").permitAll();
+                    // 관리자 로그인 페이지 접근
                     authorizeRequest.requestMatchers("/admin/login").permitAll();
+                    // 관리자 페이지 접근
                     authorizeRequest.requestMatchers("/admin/").permitAll();
+                    // 엑셀 다운로드 링크 접근
                     authorizeRequest.requestMatchers("/admin/excel-download").permitAll();
+                    // 나머지 모두 인증 필요
                     authorizeRequest.anyRequest().authenticated();
                 })
+                // jwt 토큰 필터 적용
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
