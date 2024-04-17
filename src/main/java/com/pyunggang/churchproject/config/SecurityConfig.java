52defc850bf3105b1dd16dee8fbd37d18465c14a
LeeWonHee <pigmong02020202@gmail.com>
2024-04-17 20:15:05 +0900

    https certbot 인증 주소 예외처리

--- src/main/java/com/pyunggang/churchproject/config/SecurityConfig.java
+++ src/main/java/com/pyunggang/churchproject/config/SecurityConfig.java
@@ -47,6 +47,8 @@ public class SecurityConfig {
                     authorizeRequest.requestMatchers("/refresh").permitAll();
                     // static 파일 접근 허용
                     authorizeRequest.requestMatchers("/css/*").permitAll();
+                    // https standalone open
+                    authorizeRequest.requestMatchers("/.well-known/*").permitAll();
                     // 나머지 모두 인증 필요
                     authorizeRequest.anyRequest().authenticated();
                 })
