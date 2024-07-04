97656fa1ece62f8adbbab8cee0ba41b4071555d6
LeeWonHee <pigmong02020202@gmail.com>
2024-07-04 21:50:53 +0900

    교회 이름 불러오기 Param 생성

---
+++ src/main/java/com/pyunggang/churchproject/data/dto/GetChurchNameParam.java
@@ -0,0 +1,15 @@
+package com.pyunggang.churchproject.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GetChurchNameParam {
+    private String churchName;
+    private boolean isServerOpen;
+}
