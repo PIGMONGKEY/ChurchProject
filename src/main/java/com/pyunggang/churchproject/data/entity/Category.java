a4c56f26ea8010a1e41e711bffc15476b6628411
LeeWonHee <pigmong02020202@gmail.com>
2024-10-05 01:19:05 +0900

    Fix: Category 엔티티에 카테고리명 누락 수정
    Category 엔티티에 카테고리명이 누락되어 추가함
    
    Related to: #1

--- src/main/java/com/pyunggang/churchproject/data/entity/Category.java
+++ src/main/java/com/pyunggang/churchproject/data/entity/Category.java
@@ -1,6 +1,7 @@
 package com.pyunggang.churchproject.data.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,10 +20,19 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(name = "category_name", nullable = false)
+    private String name;
+
     @ManyToOne
     @JoinColumn(name = "event_name", referencedColumnName = "name", nullable = false)
     private Event event;
 
     @OneToMany(mappedBy = "category")
     private List<Applyment> applyments = new ArrayList<>();
+
+    @Builder
+    public Category(String name, Event event) {
+        this.name = name;
+        this.event = event;
+    }
 }
