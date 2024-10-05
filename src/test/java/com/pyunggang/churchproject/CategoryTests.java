13985337af391f1398572bc7dd153a2bab73d05f
LeeWonHee <pigmong02020202@gmail.com>
2024-10-05 15:51:14 +0900

    Feat: 카테고리 저장 기능 추가
    CategoryService를 생성하고 addCategory 메서드를 구현함
    CategoryParam을 파라미터로 받으며, 중복, Event 존재 확인의 절차를 거쳐 DB에 반영
    리턴타입은 ResponseEntity에 HttpStatus만 포함시킴
    
    중복체크, Event 존재 확인 체크 테스트 완료
    
    Related to: #1

--- src/test/java/com/pyunggang/churchproject/CategoryTests.java
+++ src/test/java/com/pyunggang/churchproject/CategoryTests.java
@@ -1,9 +1,11 @@
 package com.pyunggang.churchproject;
 
+import com.pyunggang.churchproject.data.dto.CategoryParam;
 import com.pyunggang.churchproject.data.entity.Category;
 import com.pyunggang.churchproject.data.entity.Event;
 import com.pyunggang.churchproject.data.repository.CategoryRepository;
 import com.pyunggang.churchproject.data.repository.EventRepository;
+import com.pyunggang.churchproject.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,20 +18,28 @@ public class CategoryTests {
     CategoryRepository categoryRepo;
     @Autowired
     EventRepository eventRepo;
+    @Autowired
+    CategoryService categoryService;
 
     @Test
     public void insertTest() {
-        Category category = Category.builder().name("1,2학년").event(eventRepo.findEventByNameIs("그림그리기")).build();
-        categoryRepo.save(category);
+//        Category category = Category.builder().name("1,2학년").event(eventRepo.findEventByNameIs("그림그리기")).build();
+//        categoryRepo.save(category);
+
+//        // 성공, 중복 테스트
+//        System.out.println(categoryService.addCategory(new CategoryParam("3,4학년", "그림그리기")).getStatusCode());
+
+//        // 이벤트 없음 테스트
+//        System.out.println(categoryService.addCategory(new CategoryParam("3,4학년", "없는종목")).getStatusCode());
     }
 
     @Test
     public void deleteTest() {
-        categoryRepo.deleteById(2);
+//        categoryRepo.deleteById(2);
     }
 
     @Test
     public void existsTest() {
-        System.out.println(categoryRepo.existsByNameAndEventName("1, 2학년", "글짓기"));
+//        System.out.println(categoryRepo.existsByNameAndEventName("1, 2학년", "글짓기"));
     }
 }
