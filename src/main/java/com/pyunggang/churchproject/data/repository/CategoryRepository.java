58722fb2bd3793c788b2484c56a7d53a884a036a
LeeWonHee <pigmong02020202@gmail.com>
2024-10-05 18:59:08 +0900

    Feat: CategoryRepository 카테고리명, 종목명으로 조회 메서드 추가
    Applyment 저장을 위해 Category를 조회해서 불러올 필요가 있음
    이를 위해 카테고리 조회 메서드를 추가함
    
    Related to: #1

--- src/main/java/com/pyunggang/churchproject/data/repository/CategoryRepository.java
+++ src/main/java/com/pyunggang/churchproject/data/repository/CategoryRepository.java
@@ -4,9 +4,11 @@ import com.pyunggang.churchproject.data.entity.Category;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Optional<Category> findByNameAndEventName(String categoryName, String eventName);
     boolean existsByNameAndEventName(String categoryName, String eventName);
 
     @Transactional
