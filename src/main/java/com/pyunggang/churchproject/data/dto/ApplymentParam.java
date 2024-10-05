473a0cd12588a2ed737b125eec59fd9addbf27c0
LeeWonHee <pigmong02020202@gmail.com>
2024-10-05 18:27:02 +0900

    Feat: ApplymentParam DTO에 categoryName 항목 추가
    Category 엔티티를 추가함에 따라 신청 정보를 받는 ApplymentParam에 categoryName필드를 추가함
    
    Related to: #3

--- src/main/java/com/pyunggang/churchproject/data/dto/ApplymentParam.java
+++ src/main/java/com/pyunggang/churchproject/data/dto/ApplymentParam.java
@@ -12,6 +12,7 @@ public class ApplymentParam {
     private String churchName;
     private String eventName;
     private String department;
+    private String categoryName;
     private String name;
     private int age;
     private int grade;
@@ -34,6 +35,7 @@ public class ApplymentParam {
         this.churchName = applyment.getParticipant().getChurch().getName();
         this.eventName = applyment.getEvent().getName();
         this.department = applyment.getParticipant().getDepartment().getName();
+        this.categoryName = applyment.getCategory().getName();
         this.name = applyment.getParticipant().getName();
         this.age = applyment.getParticipant().getAge();
         this.grade = applyment.getParticipant().getGrade();
